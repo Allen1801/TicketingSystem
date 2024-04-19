fb65a7f9b8eb133e092a0681e75b13b558ab4352
Allen1801 <james.sangabriel@gmail.com>
2024-04-19 10:42:14 +0800

    testing

--- resources/views/action.blade.php
+++ resources/views/action.blade.php
@@ -3,6 +3,6 @@
 </a>
 <a href="javascript:void(0)" data-toggle="tooltip" onClick="delFunc({{ $id }})" data-original-title="Delete" class="edit btn btn-danger bi bi-trash edit">
 </a>
-<!-- <a href="javascript:void(0)" data-toggle="tooltip" onClick="noteFunc({{ $id }})" data-original-title="Delete" class="edit btn btn-primary bi bi-journal-medical edit">
-</a> -->
+<a href="javascript:void(0)" data-toggle="tooltip" onClick="noteFunc({{ $id }})" data-original-title="Delete" class="edit btn btn-primary bi bi-journal-medical edit">
+</a>
 
